a5b7060a5afda57f232aca76e5839f766ac7ac31
johntravise <137225982+johntravise@users.noreply.github.com>
2023-07-24 11:11:59 -0700

    Create calcdata.py

---
+++ calcdata.py
@@ -0,0 +1,66 @@
+import argparse
+import pandas as pd
+
+def load_data(file_path):
+    # Load the Excel file
+    df = pd.read_excel(file_path)
+    return df
+
+def find_top_senders(df, output_file):
+    # Get the counts of unique values in the 'Sender Email Address' column
+    email_counts = df['Sender Email Address'].value_counts()
+
+    # Get the top 10 most common email addresses
+    top_10_senders = email_counts.head(10)
+
+    # Write the results to the output file
+    with open(output_file, 'w') as f:
+        f.write("Top 10 senders:\n")
+        for sender, count in top_10_senders.items():
+            f.write(f"{sender}: {count}\n")
+
+def analyze_pci(df, output_file):
+    pci_data = df[df['Matched Rules'].str.contains('PCI', na=False)]
+    pci_count = len(pci_data)
+    unique_pci_senders = pci_data['Sender Email Address'].nunique()
+
+    # Write the results to the output file
+    with open(output_file, 'a') as f:
+        f.write(f"\nPCI related emails: {pci_count}\n")
+        f.write(f"Unique senders of PCI related emails: {unique_pci_senders}\n")
+
+def quarantine_analysis(df, output_file):
+    total_quarantined = df['Quarantined State'].sum()
+    total_released = df['Restored From Quarantine State'].sum()
+    total_not_released = total_quarantined - total_released
+
+    # Write the results to the output file
+    with open(output_file, 'a') as f:
+        f.write(f"\nTotal emails quarantined: {total_quarantined}\n")
+        f.write(f"Total emails released from quarantine: {total_released}\n")
+        f.write(f"Total emails not released from quarantine: {total_not_released}\n")
+
+def analyze_quarantine_by_brand(df, output_file):
+    df['Sender Domain'] = df['Sender Email Address'].str.split('@').str[1]
+    quarantined_by_brand = df[df['Quarantined State'] == True]['Sender Domain'].value_counts()
+    released_by_brand = df[df['Restored From Quarantine State'] == True]['Sender Domain'].value_counts()
+
+    # Write the results to the output file
+    with open(output_file, 'a') as f:
+        f.write("\nEmails quarantined by brand:\n")
+        f.write(quarantined_by_brand.to_string())
+        f.write("\nEmails released from quarantine by brand:\n")
+        f.write(released_by_brand.to_string())
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Analyze email data.')
+    parser.add_argument('input_file', type=str, help='Input Excel file')
+    parser.add_argument('output_file', type=str, nargs='?', default='results.txt', help='Output text file (default: results.txt)')
+
+    args = parser.parse_args()
+
+    df = load_data(args.input_file)
+    find_top_senders(df, args.output_file)
+    analyze_pci(df, args.output_file)
+    quarantine_analysis(df, args.output_file)
+    analyze_quarantine_by_brand(df, args.output_file)
