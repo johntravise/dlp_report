0b54693113904e83f6d020c6c8033a88a8d76af1
John Travise <johnt@johnt-mbp.lan>
2023-07-25 15:26:08 -0700

    fancified

--- calcdata.py
+++ calcdata.py
@@ -1,29 +1,16 @@
 import argparse
 import pandas as pd
-from docx import Document
 
 
-def text_to_docx(text_file, docx_file):
-    doc = Document()
-    with open(text_file, 'r') as file:
-        for line in file:
-            doc.add_paragraph(line)
-    doc.save(docx_file)
-
-
-def load_data(file_path):
-    # Load the Excel file
-    df = pd.read_excel(file_path)
+def load_data(file_paths):
+    df = pd.concat((pd.read_excel(file) for file in file_paths))
     return df
 
-def find_top_senders(df, output_file):
-    # Get the counts of unique values in the 'Sender Email Address' column
-    email_counts = df['Sender Email Address'].value_counts()
 
-    # Get the top 10 most common email addresses
+def find_top_senders(df, output_file):
+    email_counts = df['Sender Email Address'].value_counts()
     top_10_senders = email_counts.head(10)
 
-    # Write the results to the output file
     with open(output_file, 'w') as f:
         f.write("********** TOP TEN SENDERS BEGIN **********\n")
         f.write("Top 10 senders:\n")
@@ -31,43 +18,42 @@ def find_top_senders(df, output_file):
             f.write(f"{sender}: {count}\n")
         f.write("********** TOP TEN SENDERS END **********\n\n\n")
 
+
 def analyze_pci(df, output_file):
     pci_data = df[df['Matched Rules'].str.contains('PCI', na=False)]
     pci_count = len(pci_data)
     unique_pci_senders = pci_data['Sender Email Address'].unique()
 
-    # Write the results to the output file
     with open(output_file, 'a') as f:
         f.write("********** PCI BEGIN **********\n")
         f.write(f"\nPCI related emails: {pci_count}\n")
         f.write(f"Unique senders of PCI related emails: {len(unique_pci_senders)}\n\n\n")
-        f.write("List of unique senders of PCI related emails with subject and found text:\n")
+        f.write("List of unique senders of PCI related emails with subject and Message ID:\n")
         for sender in unique_pci_senders:
             sender_data = pci_data[pci_data['Sender Email Address'] == sender]
             for _, row in sender_data.iterrows():
-                f.write(f"Sender: {sender}, Subject: {row['Subject']}, MessageID: {row['Message ID']}\n")
+                f.write(f"Sender: {sender}, Subject: {row['Subject']}, Message ID: {row['Message ID']}\n")
         f.write("********** PCI END **********\n\n\n")
 
+
 def quarantine_analysis(df, output_file):
-    total_quarantined = df['Quarantined State'].sum()
-    total_released = df['Restored From Quarantine State'].sum()
+    total_quarantined = len(df)
+    total_released = df['Quarantined Restored Date'].notna().sum()
     total_not_released = total_quarantined - total_released
 
-    # Write the results to the output file
     with open(output_file, 'a') as f:
         f.write("********** QUARANTINE/RELEASE BEGIN **********\n")
-
         f.write(f"\nTotal emails quarantined: {total_quarantined}\n")
         f.write(f"Total emails released from quarantine: {total_released}\n")
         f.write(f"Total emails not released from quarantine: {total_not_released}\n")
         f.write("********** QUARANTINE/RELEASE END **********\n\n\n")
 
+
 def analyze_quarantine_by_brand(df, output_file):
     df['Sender Domain'] = df['Sender Email Address'].str.split('@').str[1]
-    quarantined_by_brand = df[df['Quarantined State'] == True]['Sender Domain'].value_counts()
-    released_by_brand = df[df['Restored From Quarantine State'] == True]['Sender Domain'].value_counts()
+    quarantined_by_brand = df['Sender Domain'].value_counts()
+    released_by_brand = df[df['Quarantined Restored Date'].notna()]['Sender Domain'].value_counts()
 
-    # Write the results to the output file
     with open(output_file, 'a') as f:
         f.write("********** QUARANTINE BY BRAND BEGIN **********\n")        
         f.write("\nEmails quarantined by brand:\n")
@@ -76,16 +62,16 @@ def analyze_quarantine_by_brand(df, output_file):
         f.write(released_by_brand.to_string())
         f.write("\n********** QUARANTINE BY BRAND END **********\n\n\n")    
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Analyze email data.')
-    parser.add_argument('input_file', type=str, help='Input Excel file')
-    parser.add_argument('output_file', type=str, nargs='?', default='results.txt', help='Output text file (default: results.txt)')
+    parser.add_argument('input_files', type=str, nargs='+', help='Input Excel files')
+    parser.add_argument('output_file', type=str, help='Output text file')
 
     args = parser.parse_args()
 
-    df = load_data(args.input_file)
+    df = load_data(args.input_files)
     find_top_senders(df, args.output_file)
     analyze_pci(df, args.output_file)
     quarantine_analysis(df, args.output_file)
     analyze_quarantine_by_brand(df, args.output_file)
-    text_to_docx(args.output_file, 'results.docx')
--- datatoexcel.py
+++ datatoexcel.py
@@ -2,15 +2,18 @@ import argparse
 import pandas as pd
 import os
 
-def load_data(file_path):
-    _, file_extension = os.path.splitext(file_path)
-    if file_extension == '.csv':
-        df = pd.read_csv(file_path)
-    elif file_extension in ['.xls', '.xlsx']:
-        df = pd.read_excel(file_path)
-    else:
-        raise ValueError(f"Unsupported file type: {file_extension}")
-    return df
+def load_data(file_paths):
+    dfs = []
+    for file_path in file_paths:
+        _, file_extension = os.path.splitext(file_path)
+        if file_extension == '.csv':
+            df = pd.read_csv(file_path)
+        elif file_extension in ['.xls', '.xlsx']:
+            df = pd.read_excel(file_path)
+        else:
+            raise ValueError(f"Unsupported file type: {file_extension}")
+        dfs.append(df)
+    return pd.concat(dfs)
 
 def find_top_senders(df):
     email_counts = df['Sender Email Address'].value_counts()
@@ -26,14 +29,13 @@ def analyze_pci(df):
     for sender in unique_pci_senders:
         sender_data = pci_data[pci_data['Sender Email Address'] == sender]
         for _, row in sender_data.iterrows():
-            result = pd.concat([result, pd.DataFrame({'Sender': [sender], 'Subject': [row['Subject']], 'Found Text': [row['Found Text']]})], ignore_index=True)
+            result = pd.concat([result, pd.DataFrame({'Sender': [sender], 'Subject': [row['Subject']], 'Message ID': [row['Message ID']]})], ignore_index=True)
     
     return result
 
-
 def quarantine_analysis(df):
-    total_quarantined = df['Quarantined State'].sum()
-    total_released = df['Restored From Quarantine State'].sum()
+    total_quarantined = len(df)
+    total_released = df['Quarantined Restored Date'].notna().sum()
     total_not_released = total_quarantined - total_released
 
     result = pd.DataFrame({
@@ -46,22 +48,25 @@ def quarantine_analysis(df):
 
 def analyze_quarantine_by_brand(df):
     df['Sender Domain'] = df['Sender Email Address'].str.split('@').str[1]
-    quarantined_by_brand = df[df['Quarantined State'] == True]['Sender Domain'].value_counts()
-    released_by_brand = df[df['Restored From Quarantine State'] == True]['Sender Domain'].value_counts()
+    quarantined_by_brand = df['Sender Domain'].value_counts()
+    released_by_brand = df[df['Quarantined Restored Date'].notna()]['Sender Domain'].value_counts()
 
     result = pd.concat([quarantined_by_brand, released_by_brand], axis=1, keys=['Quarantined', 'Released'])
     return result.reset_index().rename(columns={'index': 'Brand'})
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Analyze email data.')
-    parser.add_argument('input_file', type=str, help='Input Excel or CSV file')
-    parser.add_argument('output_file', type=str, nargs='?', default='results.xlsx', help='Output Excel file (default: results.xlsx)')
+    parser.add_argument('files', type=str, nargs='+', help='One or more input Excel or CSV files followed by the output Excel file')
 
     args = parser.parse_args()
 
-    df = load_data(args.input_file)
+    # Split input and output files
+    input_files = args.files[:-1]
+    output_file = args.files[-1]
 
-    with pd.ExcelWriter(args.output_file) as writer:
+    df = load_data(input_files)
+
+    with pd.ExcelWriter(output_file) as writer:
         find_top_senders(df).to_excel(writer, sheet_name='Top Senders', index=False)
         analyze_pci(df).to_excel(writer, sheet_name='PCI Analysis', index=False)
         quarantine_analysis(df).to_excel(writer, sheet_name='Quarantine Analysis', index=False)
